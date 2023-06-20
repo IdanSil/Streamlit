ecef979ffac81f313153c17eb34e902b81412256
IdanSil <73306250+IdanSil@users.noreply.github.com>
2023-06-20 14:39:12 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -25,7 +25,7 @@ def page1():
   add_bg_from_url()
   st.title("Learlir & Dandan")
   st.header(':heart: 2 Amazing Years Together :heart:')
-  st.image('https://i.ibb.co/7rBQv9V/camel-removebg-preview.png', width=500)
+  st.image('https://i.ibb.co/7rBQv9V/camel-removebg-preview.png', width=150)
   st.subheader('Our Story, by ChatGPT')
   relationship_summary = "In the summer of 2021, your eyes met for the first time at IDC, and it wasn't long before you shared your first kiss and official date at the Municipal Bar. Your bond deepened over shared laughter on your second date at Yam Bar, and during the third date, a pizza night at Lear's old apartment, your connection became undeniable. A thrilling movie night and a first sleepover paved the way to a stronger relationship. Amid these beautiful moments, you expressed your love for each other, an emotion beautifully captured during a documented ride together. Before embarking on a long-distance journey of five months, you took a memorable trip to Mitzpe Ramon, creating cherished memories to hold on to during your time apart. During the long-distance period, while you were studying in the US and Lear was in Israel, Lear visited you in Madrid, symbolizing your dedication to each other. Upon reuniting, you began a thrilling chapter of adventures, traveling to Rome and Greece, moving in together, and even adopting a 'doggy daughter'. Over the span of these two years, your love story unfolded, marked by shared adventures, new beginnings, and enduring love that transcends distance."
   st.write(relationship_summary)
@@ -39,7 +39,7 @@ def page2():
       st.subheader(row['event'])
       st.write('Date: ', row['date'])
       st.write('Location: ', row['place'])
-      st.image(row['image_link'],width=1000)
+      st.image(row['image_link'],width=360)
       st.write(row['notes'])
       st.markdown("---")  # Line separator
 
@@ -60,7 +60,7 @@ def page3():
       folium.Marker(location=[row['latitude'],row['longitude']],
                     popup = popup, c=row['event']).add_to(m)
 
-  folium_static(m, width=1000)
+  folium_static(m, width=360)
 
 pages = {
     "Home": page1,
