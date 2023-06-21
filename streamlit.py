6c461b163bf0d615b562dd5e1d1e86a7a5ebfa47
IdanSil <73306250+IdanSil@users.noreply.github.com>
2023-06-22 02:28:18 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -132,6 +132,7 @@ def page4():
     st.write("I bet you were wondering this whole time - Ok... 'but how can we exist without a proper logo?', Well as usual you're right, there it is!")
     if st.button("Show me already"):
         st.image(LOGO_LINK,width=IMAGE_WIDTH)
+        st.write("TA-DAA! Love You :heart:")
     
 
 pages = {
