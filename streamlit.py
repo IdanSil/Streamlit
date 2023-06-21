14aa9f5130457844890e6f6ca65596946a8a730a
IdanSil <73306250+IdanSil@users.noreply.github.com>
2023-06-22 00:57:04 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -45,8 +45,15 @@ def add_bg_from_url():
          unsafe_allow_html=True
      )
 
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-df = pd.read_csv('Firsts - st.csv')
+df = load_data(st.secrets["public_gsheets_url"])
+
 
 def page1():
     add_logo()
