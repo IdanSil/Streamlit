0293bd4c000355b897bca1d2f04d663ccab479bc
IdanSil <73306250+IdanSil@users.noreply.github.com>
2023-06-22 02:24:40 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -127,7 +127,8 @@ def page4():
 
     st.write(message)
     st.markdown("---")
-    st.subheader('Keep Reading... We're almost done :joy:')
+    
+    st.subheader("Keep Reading... We're almost done :joy:")
     st.write("I bet you were wondering this whole time - Ok... 'but how can we exist without a proper logo?', Well as usual you're right, there it is!")
     st.image(LOGO_LINK,width=IMAGE_WIDTH)
     
