37f849a21d04274e2252caee4996969a250058b4
IdanSil <73306250+IdanSil@users.noreply.github.com>
2023-06-22 01:56:24 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,8 +5,9 @@ import folium
 from streamlit_folium import st_folium, folium_static
 
 IMAGE_WIDTH = 300
-BG_LINK = 'https://img.freepik.com/premium-vector/seamless-horizontal-tropical-background-hand-drawn-palm-trees-sketch-exotic-tropic-jungle-leaves-paradise-palm-tree-wallpaper-illustration_102902-2665.jpg'
+BG_LINK = 'https://cdn.shopify.com/s/files/1/0020/6123/8339/products/6622379835438_720x.jpg'
 LOGO_LINK = 'https://i.ibb.co/7rBQv9V/camel-removebg-preview.png'
+FIRST_PAGE_LINK = 'https://i.etsystatic.com/icm/44e445/558970353/icm_fullxfull.558970353_nt195fa87j4gswwwkcgw.jpg'
     
 def add_bg_from_url():
     st.markdown(
@@ -33,7 +34,7 @@ def add_logo():
                 background-position: 20px 20px;
             }
             [data-testid="stSidebarNav"]::before {
-                content: "My Company Name";
+                content: "Anniversary";
                 margin-left: 20px;
                 margin-top: 20px;
                 font-size: 30px;
@@ -56,15 +57,17 @@ df = load_data(st.secrets["public_gsheets_url"])
 
 
 def page1():
+    add_logo()
     add_bg_from_url()
     st.title("Learlir & Dandan")
     st.header(':heart: 2 Amazing Years Together :heart:')
-    st.image('https://i.ibb.co/7rBQv9V/camel-removebg-preview.png', width=IMAGE_WIDTH)
+    st.image(FIRST_PAGE_LINK, width=IMAGE_WIDTH)
     st.subheader('Our Story, by ChatGPT')
     relationship_summary = "In the summer of 2021, your eyes met for the first time at IDC, and it wasn't long before you shared your first kiss and official date at the Municipal Bar. Your bond deepened over shared laughter on your second date at Yam Bar, and during the third date, a pizza night at Lear's old apartment, your connection became undeniable. A thrilling movie night and a first sleepover paved the way to a stronger relationship. Amid these beautiful moments, you expressed your love for each other, an emotion beautifully captured during a documented ride together. Before embarking on a long-distance relationship phase of five months, you took a memorable trip to Mitzpe Ramon, creating cherished memories to hold on to during your time apart. During the long-distance period, Lear visited Idan in Madrid, symbolizing your dedication to each other. Upon reuniting, you began a thrilling chapter of adventures, traveling to Rome and Greece, moving in together, and even adopting a 'doggy daughter' named 'Lulu'. Over the span of these two years, your love story unfolded, marked by shared adventures, new beginnings, and enduring love that transcends distance."
     st.write(relationship_summary)
 
 def page2():
+    add_logo()
     add_bg_from_url()
     st.title("Learlir & Dandan")
     st.header('Our Story')
@@ -91,6 +94,7 @@ def page2():
             st.markdown("---")  # Line separator
 
 def page3():
+    add_logo()
     add_bg_from_url()
     m = folium.Map(location=[df.latitude.mean(), df.longitude.mean()], 
                  zoom_start=3, control_scale=True, width=IMAGE_WIDTH)
@@ -109,10 +113,20 @@ def page3():
 
     folium_static(m, width=IMAGE_WIDTH)
 
+def page4():
+    add_bg_from_url()
+    st.title("Learlir & Dandan")
+    st.header('Another Beautiful Year Together')
+
+    message = "Happy anniversary, Lear! As we reflect on our journey over the past two years, I am overwhelmed with gratitude and love. The memories we have created together, from our first meeting at IDC to our adventures in Rome and Greece, have been nothing short of magical. Your presence in my life fills each day with joy and warmth. I am excited for the countless adventures and beautiful moments that lie ahead. Cheers to another year of love, growth, and shared happiness. I love you, always and forever."
+
+    st.write(message)
+
 pages = {
     "Home": page1,
     "Timeline": page2,
     "Map": page3
+    "Invitation": page4
 }
 
 # Render the page navigation in the sidebar
